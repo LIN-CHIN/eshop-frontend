6b1c4a4a60ae509a88ac170ccaf5189ffee27fd2
LIN-CHIN <s235068267@gmail.com>
2023-11-07 11:54:54 +0800

    feat: initial project

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button class="btn btn-primary">
+      Click me
+    </button>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const year: number = 2022
+const title: string = `${year} iThome 鐵人賽`
+</script>
