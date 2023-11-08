fd0443f58cc68a6a473843b5bf0be67d8a7b93be
LIN-CHIN <s235068267@gmail.com>
2023-11-08 10:06:04 +0800

    feat: install bootstrap and use it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  typescript: {
-    typeCheck: true
-  },
-  css: [
+  css:[
     'bootstrap/dist/css/bootstrap.css'
   ],
-  plugins: [
-    { src: '~/plugins/bootstrap.js', mode: 'client' }
+  plugins:[
+    { src: '~/plugins/bootstrap.js', mode: 'client'}
   ]
 })
